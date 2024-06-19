9c6b5ed8953cf875c94302bc396f625c1261ea52
Ahmedalghaili <ahmedalghili74@gmail.com>
2024-06-19 20:03:07 +0700

    chart

---
+++ my-project/src/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+// Dashboard.jsx
+import React from 'react';
+import {Chart} from './Chart.jsx' 
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Chart />
+    </div>
+  );
+}
+
+export default Dashboard;
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Correct path for scanning Tailwind CSS classes
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
